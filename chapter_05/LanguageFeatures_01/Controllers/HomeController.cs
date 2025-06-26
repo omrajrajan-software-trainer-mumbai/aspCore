afb98ae6393ec611c9555cae914093bb924bec7f
ashwin-pillai-programmer <iamashp17@gmail.com>
2025-06-27 03:24:47 +0530

    adding a new project "LanguageFeatures_01" in chapter_05

---
+++ chapter_05/LanguageFeatures_01/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿// using Microsoft.AspNetCore.Mvc;
+// using LanguageFeatures_01.Models;
+
+namespace LanguageFeatures_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product[] products = Product.GetProducts();
+            return View(new string[] { products[0].Name });
+        }
+    }
+}
---
+++ chapter_05/LanguageFeatures_01/GlobalUsings.cs
@@ -0,0 +1,2 @@
+﻿global using LanguageFeatures_01.Models;
+global using Microsoft.AspNetCore.Mvc;

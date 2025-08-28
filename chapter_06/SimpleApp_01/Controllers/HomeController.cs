68bc10b045fb99b8ad7445197e3d22044b212c4f
ashwin-pillai-programmer <iamashp17@gmail.com>
2025-08-28 15:27:16 +0530

    Creating a new test project SimpleApp_01.test based on the project SimpleApp_01 by ashwin

---
+++ chapter_06/SimpleApp_01/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApp_01.Models;
+namespace SimpleApp_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View(Product.GetProducts());
+        }
+    }
+}
---
+++ chapter_06/SimpleApp_01/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleApp_01.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal? Price { get; set; }
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product
+            {
+                Name = "Ashwin",
+                Price = 275M
+            };
+            Product lifejacket = new Product
+            {
+                Name = "Lifejacket",
+                Price = 48.95M
+            };
+            return new Product[] { kayak, lifejacket };
+        }
+    }
+}

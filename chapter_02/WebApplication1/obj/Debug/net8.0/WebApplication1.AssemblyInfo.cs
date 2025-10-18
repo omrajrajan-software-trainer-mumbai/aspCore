24b038fb4ee6ae01a8cd64fb906ff883587ee1a6
Omraj Rajan <omrajrajan@gmail.com>
2025-10-18 19:44:58 +0530

    removed project webApplication1 from the solution Chapter_02

--- chapter_02/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ chapter_02/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+780612acfee791e89b148e68ee29317a1c4c8ce3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e10414b1881bf72d8840ed10655a2cb86626a4f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

67795961c762c1b2a0c7353b1e13ae7c4bba99b5
ashwin-pillai-programmer <iamashp17@gmail.com>
2025-10-28 20:27:09 +0530

    Adding new project to the solution "Chapter_04"

---
+++ chapter_04/MyProject_01/Program.cs
@@ -0,0 +1,32 @@
+namespace MyProject_01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello Ashwin!");
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}

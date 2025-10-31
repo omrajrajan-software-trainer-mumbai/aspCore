c3adbdf6410b657cbbe62f7df9b89679e0887648
ashwin-pillai-programmer <iamashp17@gmail.com>
2025-10-31 15:18:35 +0530

    added some updates in the solution chapter_02

---
+++ chapter_02/FirstProject_02/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using FirstProject_02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstProject_02.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView", viewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ chapter_02/FirstProject_02/Views/Home/MyView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstProject_02.Views.Home
+{
+    public class MyViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
